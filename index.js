b50a84ee98d78399f043ca4ebd5715be2845c5e1
Scott Hall <scotthall127@gmail.com>
2018-04-20 04:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`
+}
